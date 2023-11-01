da64e6f0b62cbb0cdedb61da1d1e90bed0f6410c
ChooSeongWoo <sldoal2423@gmail.com>
2023-11-02 00:15:06 +0900

    FIX :: 데이터셋 및 map 함수 DirectList와 GroupList 데이터셋을 분리 Chat.jsx에서 DirectList map 돌림

--- src/components/Sidebar/DirectChatList.jsx
+++ src/components/Sidebar/DirectChatList.jsx
@@ -1,35 +1,31 @@
-import React from 'react';
-import { faker } from '@faker-js/faker';
+import React from "react";
+import { faker } from "@faker-js/faker";
 
-import { BsCheckCircleFill } from 'react-icons/bs';
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const DirectChatList = () => {
-	return (
-		<div className='DirectChat_FriendList_Container'>
-			<div className='DirectChat_FriendList'>
-				<div className='DirectChat_Left'>
-					<img
-						src={faker.image.avatar()}
-						alt=''
-						className='Avatar_Img'
-					/>
-					<div className='FriendList_Info'>
-						<p className='Info_Name'>Direct</p>
-						<p className='Info_LastMessage'>Hey, Kris wayd?</p>
-					</div>
-				</div>
+  return (
+    <div className="DirectChat_FriendList_Container">
+      <div className="DirectChat_FriendList">
+        <div className="DirectChat_Left">
+          <img src={faker.image.avatar()} alt="" className="Avatar_Img" />
+          <div className="FriendList_Info">
+            <p className="Info_Name">Direct</p>
+            <p className="Info_LastMessage">Hey, Kris wayd?</p>
+          </div>
+        </div>
 
-				<div className='DirectChat_Right'>
-					<div className='MessageState'>
-						<p className='MessageState_Time'>12:45 PM</p>
-						<div className='MessageState_Time_Icon'>
-							<BsCheckCircleFill size={20} />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="DirectChat_Right">
+          <div className="MessageState">
+            <p className="MessageState_Time">12:45 PM</p>
+            <div className="MessageState_Time_Icon">
+              <BsCheckCircleFill size={20} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DirectChatList;
