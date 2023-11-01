c8130b9a4dcf6c036cf4a8d91efdd7c3580e80c3
Jamkris <dltmdgus1412@gmail.com>
2023-11-02 08:52:57 +0900

    FIX :: 채팅리스트 스타일 정렬안맞는거 수정

--- src/components/Sidebar/DirectChatList.jsx
+++ src/components/Sidebar/DirectChatList.jsx
@@ -1,35 +1,38 @@
-import React from "react";
-import { faker } from "@faker-js/faker";
+import React from 'react';
 
-import { BsCheckCircleFill } from "react-icons/bs";
+import { BsCheckCircleFill } from 'react-icons/bs';
 
 const DirectChatList = ({ img, name, msg, time, unread, online }) => {
-  return (
-    <div className="DirectChat_FriendList_Container">
-      <div className="DirectChat_FriendList">
-        <div className="DirectChat_Left">
-          <img src={img} alt="" className="Avatar_Img" />
-          <div className="FriendList_Info">
-            <p className="Info_Name">{name}</p>
-            <p className="Info_LastMessage">{msg}</p>
-          </div>
-        </div>
+	return (
+		<div className='DirectChat_FriendList_Container'>
+			<div className='DirectChat_FriendList'>
+				<div className='DirectChat_Left'>
+					<img src={img} alt='' className='Avatar_Img' />
+					<div className='FriendList_Info'>
+						<p className='Info_Name'>{name}</p>
+						<p className='Info_LastMessage'>{msg}</p>
+					</div>
+				</div>
 
-        <div className="DirectChat_Right">
-          <div className="MessageState">
-            <p className="MessageState_Time">{time}</p>
-            {unread == 0 ? (
-              <div className="MessageState_Time_Icon">
-                <BsCheckCircleFill size={20} />
-              </div>
-            ) : (
-              <div>5</div>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className='DirectChat_Right'>
+					<div className='MessageState'>
+						<p className='MessageState_Time'>{time}</p>
+						{unread === 0 ? (
+							<div className='MessageState_Time_Icon'>
+								<BsCheckCircleFill size={20} />
+							</div>
+						) : (
+							<div className='MessageState_Unread_Container'>
+								<div className='MessageState_Unread'>
+									{unread}
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DirectChatList;
