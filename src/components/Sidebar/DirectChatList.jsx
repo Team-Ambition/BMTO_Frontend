caa8d81b7cc6f707c9e8d66149cf6f157a689e92
ChooSeongWoo <sldoal2423@gmail.com>
2023-11-02 01:00:49 +0900

    ADD :: DirectList 값  props 받은 값으로 변경 수정해야할 것: unread, CSS

--- src/components/Sidebar/DirectChatList.jsx
+++ src/components/Sidebar/DirectChatList.jsx
@@ -3,24 +3,28 @@ import { faker } from "@faker-js/faker";
 
 import { BsCheckCircleFill } from "react-icons/bs";
 
-const DirectChatList = () => {
+const DirectChatList = ({ img, name, msg, time, unread, online }) => {
   return (
     <div className="DirectChat_FriendList_Container">
       <div className="DirectChat_FriendList">
         <div className="DirectChat_Left">
-          <img src={faker.image.avatar()} alt="" className="Avatar_Img" />
+          <img src={img} alt="" className="Avatar_Img" />
           <div className="FriendList_Info">
-            <p className="Info_Name">Direct</p>
-            <p className="Info_LastMessage">Hey, Kris wayd?</p>
+            <p className="Info_Name">{name}</p>
+            <p className="Info_LastMessage">{msg}</p>
           </div>
         </div>
 
         <div className="DirectChat_Right">
           <div className="MessageState">
-            <p className="MessageState_Time">12:45 PM</p>
-            <div className="MessageState_Time_Icon">
-              <BsCheckCircleFill size={20} />
-            </div>
+            <p className="MessageState_Time">{time}</p>
+            {unread == 0 ? (
+              <div className="MessageState_Time_Icon">
+                <BsCheckCircleFill size={20} />
+              </div>
+            ) : (
+              <div>5</div>
+            )}
           </div>
         </div>
       </div>
