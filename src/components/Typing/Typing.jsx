5455b578e294090bee4db0e0a09db9e6da832b90
Jamkris <dltmdgus1412@gmail.com>
2023-11-05 23:43:42 +0900

    BUG :: 채팅 치는 곳 모양 조정 안됌

--- src/components/Typing/Typing.jsx
+++ src/components/Typing/Typing.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import { BsMic } from "react-icons/bs";
-import { CiImageOn } from "react-icons/ci";
-import { AiOutlinePaperClip } from "react-icons/ai";
+import React from 'react';
+import { BsMic } from 'react-icons/bs';
+import { CiImageOn } from 'react-icons/ci';
+import { AiOutlinePaperClip } from 'react-icons/ai';
 
-import "./CSS/Typing.css";
+import './CSS/Typing.css';
 
-const Typing = () => {
-  return (
-    <div className="Typing_Container">
-      <div className="Typing_Input">
-        <input type="text" placeholder="Message..." />
-      </div>
-      <div className="Typing_Files">
-        <BsMic size={25} className="Typing_File" />
-        <CiImageOn size={30} className="Typing_File" />
-        <AiOutlinePaperClip size={30} className="Typing_Name"/>
-      </div>
-    </div>
-  );
+const Typing = ({ isInfoOpen }) => {
+	return (
+		<div
+			className='Typing_Container'
+			id={isInfoOpen ? 'StateTypeTrue' : 'StateTypeFalse'}
+		>
+			<div className='Typing_Input'>
+				<input type='text' placeholder='Message...' />
+			</div>
+			<div className='Typing_Files'>
+				<BsMic size={25} className='Typing_File' />
+				<CiImageOn size={30} className='Typing_File' />
+				<AiOutlinePaperClip size={30} className='Typing_Name' />
+			</div>
+		</div>
+	);
 };
 
 export default Typing;
