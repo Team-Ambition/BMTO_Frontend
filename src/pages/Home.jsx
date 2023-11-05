7418e254238ee2a5f7112be2dde6a5a9cad939a1
Jamkris <dltmdgus1412@gmail.com>
2023-11-05 22:53:30 +0900

    ADD :: GroupChat Info 퍼블리싱 끝 나타나게 해야함

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import "./CSS/Home.css";
+import React, { useState } from 'react';
+import './CSS/Home.css';
 
-import Header from "../components/Header/Header";
-import Chat from "../components/Sidebar/Chat";
-import Info from "../components/Info/Info";
+import Header from '../components/Header/Header';
+import Chat from '../components/Sidebar/Chat';
+import Info from '../components/Info/Info';
 
 const Home = () => {
-  return (
-    <div className="Home_Container">
-      <Chat />
+	const [isInfoOpen, setIsInfoOpen] = useState(false);
+	return (
+		<div className='Home_Container'>
+			<Chat />
 
-      <div className="Home_Main">
-        <Header />
-      </div>
+			<div className='Home_Main'>
+				<Header />
+			</div>
 
-      <div className="Home_Chat_Info">
-      </div>
-    </div>
-  );
+			<div className='Home_Chat_Info'>
+				<Info />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
