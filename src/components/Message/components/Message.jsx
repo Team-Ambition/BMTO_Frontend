f8db21a8a936f6f05482099125a728327d524834
ChooSeongWoo <sldoal2423@gmail.com>
2023-11-08 01:22:07 +0900

    STYLE :: Message 컴포넌트 글자수에 따른 크기 변화, 줄바꿈

--- src/components/Message/components/Message.jsx
+++ src/components/Message/components/Message.jsx
@@ -6,7 +6,7 @@ const Message = () => {
   return (
     <div>
       <div className="Message_Container" id="Message_Incoming">
-        <div className="Message_Text">안녕하세요</div>
+        <div className="Message_Text">안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요</div>
       </div>
       <div className="Message_Container" id="Message_Outgoing">
         <div className="Message_Text">안녕하세요</div>
